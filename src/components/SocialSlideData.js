4c5f58fd93cd7ce15cbbdb2e685b0753f7ff7df6
Abdulrahman Kasim <abdukasimr@gmail.com>
2022-01-07 11:32:25 +0300

    Created slides and data for slides in Social mode

---
+++ src/components/SocialSlideData.js
@@ -0,0 +1,59 @@
+import {
+  FaGoogle,
+  FaFreeCodeCamp,
+  FaSwimmer,
+  FaBasketballBall,
+  FaTv,
+} from "react-icons/fa";
+
+const iconClass = "text-white text-6xl";
+
+export const SocialSlideData = [
+  {
+    title: "Interest",
+    icons: [
+      <div>
+        <FaSwimmer className={iconClass} />
+        <h4 className="text-white text-xs ml-2 mt-2">Swimming</h4>
+      </div>,
+      <div>
+        <FaBasketballBall className={iconClass} />
+        <h4 className="text-white text-xs ml-2 mt-2">Basketball</h4>
+      </div>,
+      <div>
+        <FaTv className={iconClass} />
+        <h4 className="text-white text-xs ml-2 mt-2">Watching TV</h4>
+      </div>,
+    ],
+  },
+  {
+    title: "Information",
+    icons: [
+      <div>
+        <p className="text-white">
+          Name: <span className="ml-28">Yimage</span>
+        </p>
+      </div>,
+      <div>
+        <p className="text-white">
+          Address: <span className="ml-24">Gerji</span>
+        </p>
+      </div>,
+      <div>
+        <p className="text-white">
+          Birthday: <span className="ml-20">09/03/1996</span>
+        </p>
+      </div>,
+      <div>
+        <p className="text-white">
+          Mobile: <span className="ml-20">+251901186214</span>
+        </p>
+      </div>,
+      <div>
+        <p className="text-white">
+          Social Level: <span className="ml-16">Bumble Bee</span>
+        </p>
+      </div>,
+    ],
+  },
+];
---
+++ src/components/SocialSlider.js
@@ -0,0 +1,65 @@
+import Swipe from "react-easy-swipe";
+import { useState } from "react/cjs/react.development";
+import { SocialSlideData } from "./SocialSlideData";
+
+const WorkSlider = ({ mode }) => {
+  let [currentSlide, setCurrentSlide] = useState(0);
+
+  const nextSlide = () => {
+    let newSlide =
+      currentSlide === SocialSlideData.length - 1 ? 0 : currentSlide + 1;
+    setCurrentSlide(newSlide);
+  };
+
+  const prevSlide = () => {
+    let newSlide =
+      currentSlide === 0 ? SocialSlideData.length - 1 : currentSlide - 1;
+    setCurrentSlide(newSlide);
+  };
+
+  return (
+    <div>
+      <Swipe onSwipeLeft={nextSlide} onSwipeRight={prevSlide}>
+        {SocialSlideData.map((slide, index) => {
+          return (
+            <div className={index === currentSlide ? "mt-36 ml-12" : "hidden"}>
+              <h2
+                className={
+                  index === currentSlide
+                    ? "text-4xl font-semibold text-white"
+                    : "hidden"
+                }
+              >
+                {slide.title}
+              </h2>
+
+              <div
+                className={
+                  index === currentSlide
+                    ? "flex flex-wrap overflow-hidden -mx-3  mt-16"
+                    : "hidden"
+                }
+              >
+                {slide.icons.map((icon, index) => {
+                  return (
+                    <div
+                      className={
+                        slide.title === "Interest"
+                          ? `w-1/3 overflow-hidden my-3 px-3`
+                          : `my-1 px-3 w-full overflow-hidden`
+                      }
+                    >
+                      {icon}
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          );
+        })}
+      </Swipe>
+    </div>
+  );
+};
+
+export default WorkSlider;
